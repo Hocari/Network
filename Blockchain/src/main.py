ba8b91842ff8da00ba04a33b8cc07b986f09a7db
HocariFounder <contact@hocari.com>
2023-02-07 19:54:15 -0500

    Update main.py

--- Blockchain/src/main.py
+++ Blockchain/src/main.py
@@ -66,6 +66,52 @@ class Blockchain:
             return True
         return False
 
+    def consensus(self):
+        """
+        Handles the consensus mechanism for the blockchain.
+        """
+        # code to handle consensus mechanism ...
+
+    def backup_blockchain(self):
+        """
+        Backs up the current state of the blockchain to a file.
+        """
+        # code to backup the blockchain to a file ...
+
+    def load_blockchain(self):
+        """
+        Loads a previously backed up version of the blockchain.
+        """
+        # code to load a previously backed up version of the blockchain ...
+        
+    def get_blockchain_stats(self):
+        num_blocks = len(self.chain)
+        num_transactions = 0
+        for block in self.chain:
+            num_transactions += len(block.transactions)
+        return {
+            'number of blocks': num_blocks,
+            'number of transactions': num_transactions
+            # other statistics can be added as needed
+        }
+        
+    def update_difficulty(self, new_difficulty):
+        self.difficulty = new_difficulty
+
+        
+
+
+class Token:
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+        self.difficulty_level = 2
+        Blockchain.create_genesis_block()
+        self.address_balance = {}
+        self.tokens = []
+        self.market_tokens = {}
+
+
     def create_token(self, name, symbol, total_supply):
         # Check if the token name and symbol are unique
         for token in self.tokens:
@@ -107,7 +153,7 @@ class Blockchain:
             raise ValueError(f'Token with name "{token_name}" does not exist in the market')
 
         # Check if the buyer has sufficient funds
-        buyer_balance = self.get_balance(buyer_address)
+        buyer_balance = Blockchain.get_balance(buyer_address)
         if buyer_balance < token['price'] * amount:
             raise ValueError(f'Insufficient funds for buyer with address "{buyer_address}"')
 
@@ -142,3 +188,120 @@ class Blockchain:
         # Deduct the tokens from the seller and add to the market
         token['amount'] -= amount
         self.market_tokens.append({'name': token_name, 'seller': seller_address, 'amount': amount, 'price': price})
+
+    def get_token_price(self, token_id):
+        """Returns the current price of a token with the given ID."""
+        for token in self.tokens:
+            if token['id'] == token_id:
+                return token['price']
+        return None
+
+    def get_market_tokens(self):
+        """Returns the list of all tokens available on the cryptocurrency market."""
+        return self.tokens
+
+
+
+
+class Block:
+    def __init__(self, transactions, timestamp, previous_hash, nonce=0):
+        self.transactions = transactions
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.hash = self.generate_hash()
+        self.nonce = nonce
+
+    def generate_hash(self):
+        # generate a unique hash for the block
+        # ...
+        return True
+
+    def get_transactions(self):
+        return self.transactions
+
+    def validate_block(self, previous_block_hash):
+        # check if the previous block hash is the same as the hash of the previous block
+        if self.previous_hash != previous_block_hash:
+            return False
+
+        # validate the transactions in the block
+        for transaction in self.transactions:
+            if not Transaction.validate_transaction(transaction):
+                return False
+
+        return True
+
+
+
+    
+class Transaction:
+    def validate_transaction(self, transaction):
+        # Validate the transaction
+        if not all([transaction.sender, transaction.recipient, transaction.amount]):
+            # If any of the required fields are missing, return False
+            return False
+        if transaction.amount <= 0:
+            # If the amount is not positive, return False
+            return False
+        # If the transaction passes all the checks, return True
+        return True
+
+
+
+
+class Message:
+    def __init__(self):
+        self.messages = {}
+
+    def send_message(self, recipient, message):
+        encrypted_message = self.encrypt(message)
+        self.messages[recipient] = encrypted_message
+
+    def get_message(self, recipient):
+        encrypted_message = self.messages.get(recipient)
+        if encrypted_message:
+            return self.decrypt(encrypted_message)
+        return None
+
+    def encrypt(self, message):
+        # Add code here to encrypt the message
+        return encrypted_message
+
+    def decrypt(self, encrypted_message):
+        # Add code here to decrypt the encrypted message
+        return message
+
+
+
+
+class User:
+    def __init__(self, address, username, email):
+        self.users = {}
+        self.username = username
+        self.email = email
+        self.transactions = []
+        self.address = address
+    
+    def create_user(self, address, username, email):
+        if address in self.users:
+            return "User with this address already exists."
+        
+        user = {
+            "address": address,
+            "username": username,
+            "email": email
+        }
+        self.users[address] = user
+        return "User created successfully."
+    
+    def get_user(self, address):
+        if address in self.users:
+            return self.users[address]
+        return "User not found."
+
+    def update_user(self, new_username, new_email):
+        self.username = new_username
+        self.email = new_email
+
+    def get_user_transactions(self):
+        return self.transactions
